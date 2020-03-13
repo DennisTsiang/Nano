764f066045aa70a7be882d94483b2a1586708dbe
Dennis Tsiang <dennistsiang@gmail.com>
2020-03-13 17:20:13 +0000

    Make redirect environmental agnostic

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 3000
-const azunyanFullyQualifiedURL = process.env.AZUNYAN || "http://localhost:8040"
-app.get('*', (req, res) => res.redirect(azunyanFullyQualifiedURL + req.originalUrl))
+const fullyQualifiedURL = process.env.REDIRECT || "http://localhost:8040"
+app.get('*', (req, res) => res.redirect(fullyQualifiedURL + req.originalUrl))
 
 app.listen(port, () => console.log(`Nano listening on port ${port}!`))
